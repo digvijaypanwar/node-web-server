48da097c50a60bafc1faad83d67e297f23b2aefa
Digvijay Panwar <panwardigvijay17@gmail.com>
2021-07-11 02:49:58 +0530

    New Commit

--- server.js
+++ server.js
@@ -6,60 +6,9 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
-hbs.registerPartials(__dirname+'/views/partials');
-app.set('view engine','hbs');
-app.use((req,res,next)=>{
-    var now = new Date().toString();
-    var log= `${now}: ${req.method} ${req.url}`;
-
-    console.log(log);
-    fs.appendFile('server.log',log+'\n',(err)=>{
-        if(err)
-        console.log('Unable to append log in the file!')
-    });
-
-    next();
-});
-//FOR BLOCKING THE WHOLE SITE
-// app.use((req,res,next)=>{
-//     res.render('maintenace',{
-//         header:'We\'ll be right back!',
-//         para:'Site under maintenace!'
-//     });
-// });
-app.use(express.static(__dirname+'/public'));
-
-
-hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
-hbs.registerHelper('screamIt',(text) => text.toUpperCase());
-
 app.get('/',(req,res)=>{
-    res.render('home.hbs',{
-        pageTitle:'Home Page',
-        welcomeMsg:'Hello webSurfer! \n Welcome to My website!'
-    });
+    res.send("Welcome to Djay Node Server")
 });
-
-app.get('/about',(req,res)=>{
-    res.render('about.hbs',{
-        pageTitle: 'About page'
-    });
-});
-
-app.get('/projects',(req,res)=>{
-    res.render('projects.hbs',{
-        pageTitle: 'Projects'
-    });
-});
-
-app.get('/bad',(req,res)=>{
-    res.send({
-        errorMessage:'Unable to handle the request!'
-    });
-});
-
-
-
 
 app.listen(port,()=>{
     console.log('Server is up on port',port);
