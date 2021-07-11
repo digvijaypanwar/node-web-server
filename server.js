05691c73f644e3fe56b9a5e31d1b5a217fa091b8
Digvijay Panwar <panwardigvijay17@gmail.com>
2021-07-11 15:09:45 +0530

    added /hc route

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to Djay Node Server")
+app.get("/", (req, res) => {
+    res.send("Welcome to Djay Node Server");
+});
+app.get("/hc", (req, res) => {
+    res.sendStatus(200).send("Health");
 });
 
-app.listen(port,()=>{
-    console.log('Server is up on port',port);
+app.listen(port, () => {
+    console.log("Server is up on port", port);
 });
